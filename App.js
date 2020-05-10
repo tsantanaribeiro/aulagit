91bf9cc621aae5a4514248aec45b00015c738bee
thiago.ribeiro-at-424057606549 <thiago.ribeiro@erpflex.com.br>
2020-05-10 01:00:07 -0300

    Novo app criado e novo workspace. Aula GIT

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Aula Sobre GIT App modelo.
+ */
+import express from "express";
+let app = express();
+let port = 8099;
+
+/**
+ * Quando a aplicação estiver ativa e escutando a porta 8099. Quando uma "requisiação" com o verbo GET realizada.
+ * a resposta será enviando pelo método "send".
+*/
+app .get('/', (requisicao, resposta)=>{
+    resposta.send('Olá NODE.js !! Estou me esforçando pra um Hello!');
+});
+
+/**
+ *  Prepara a plicação para escutar a porta indicada na variável "port"
+ *  */
+
+ app .listen(port,()=>{
+     console.log('Aplicação iniciada e escutando na porta:', port);
+ });
+
+ module.exports  = app;
